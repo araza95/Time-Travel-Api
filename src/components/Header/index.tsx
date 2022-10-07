b3151873c65ee95147bb8305528045db65542529
Alihaiderizvi <syedalihaiderizvi.you@gmail.com>
2022-10-08 03:45:26 +0500

    Static Header Created

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,44 @@
 import React, { FC } from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./Styles";
 
 interface IHeaderProps {}
 
 const Header: FC<IHeaderProps> = (props) => {
-  return <React.Fragment>Header</React.Fragment>;
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h5" className={classes.title}>
+            Travel Advisor
+          </Typography>
+          <Box style={{ display: "flex" }}>
+            <Typography variant="h6" className={classes.title}>
+              Explore new places
+            </Typography>
+            {/* <Autocomplete> */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+            {/* </Autocomplete> */}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 };
 
 export default Header;
